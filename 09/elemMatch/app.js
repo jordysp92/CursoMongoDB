6b2dfa043cee2b090957310be8d42803862283b9
Jordy Salas <jordysp92@gmail.com>
2019-05-15 15:05:12 -0300

    Se agrega uso de elemMatch

---
+++ 09/elemMatch/app.js
@@ -0,0 +1,26 @@
+﻿var MongoClient = require('mongodb').MongoClient;
+
+const dbName = "cursomongo"
+
+MongoClient.connect('mongodb://localhost:37017/', function(err, client) {
+	if(err) throw err;
+
+	var db = client.db(dbName);
+
+	var criterio = { 
+		puntuaciones : { 
+			$elemMatch : { 
+				tipo : "examen", 
+				puntuacion : { $gt : 90 } 
+			} 
+		} 
+	};
+	
+	db.collection('puntuaciones3').count(criterio , function(err, total) {
+		if(err) throw err;
+		
+		console.log(total);
+
+		client.close();
+	});
+});
