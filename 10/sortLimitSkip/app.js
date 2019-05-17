ff33981d39c1001bfb686654ea5ef37e8a52a94a
Jordy Salas <jordysp92@gmail.com>
2019-05-17 10:10:55 -0300

    Se agrega ordenamiento

---
+++ 10/sortLimitSkip/app.js
@@ -0,0 +1,22 @@
+﻿var MongoClient = require('mongodb').MongoClient;
+
+const dbName = "cursomongo"
+
+MongoClient.connect('mongodb://localhost:37017/', function(err, client) {
+	if(err) throw err;
+
+	var db = client.db(dbName);
+
+	var criterio = { 'tipo' : 'examen' , 'puntuacion' : { $gte : 90 } };
+	var ordenacion = { puntuacion : -1 };
+	
+	db.collection('puntuaciones').find(criterio, { puntuacion : 1 , _id : 0 }).sort(ordenacion).limit(10).skip(10).each(function(err, doc) {
+		if(err) throw err;
+		
+		if(doc == null) {
+			return client.close();
+		}
+
+		console.log(doc);
+	});
+});
